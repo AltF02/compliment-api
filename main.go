ba915f06237e922dea4238594b6986925e8436fa
Matthew <staalkoper@gmail.com>
2020-08-13 12:19:29 +0200

    working prototype

--- main.go
+++ main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"time"
 )
 
+
+func getCompliment(w http.ResponseWriter, r *http.Request)  {
+	complimentsJson, err := ioutil.ReadFile("compliments.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var compliments []string
+	_ = json.Unmarshal(complimentsJson, &compliments)
+	rand.Seed(time.Now().Unix())
+	fmt.Fprintf(w, compliments[rand.Intn(len(compliments))])
+}
+
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+	fmt.Fprintf(w, "Welcome to the compliment api, please visit github.com/DankDumpster/Compliment-api for more info")
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/compliment", getCompliment).Methods("GET")
+	fmt.Println("Server running on http://localhost:8090/")
+
+	Port := ":8090"
+	if os.Getenv("PORT") != "" {
+		Port = ":" + os.Getenv("PORT")
+	}
+	log.Fatal(http.ListenAndServe(Port, router))
 }
